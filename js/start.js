211b4db2edc4e4134792aa89a284fba2d6954172
[SW4/2반] 성치호 <clghdi9898@gmail.com>
2023-05-04 20:31:46 +0900

    fix: 버튼 클릭시 디자인 및 fadeOut 작성

--- js/start.js
+++ js/start.js
@@ -1,48 +1,59 @@
-const main = document.querySelector('#main');
-const qna = document.querySelector('#qna');
-const btn = document.querySelector('#btn');
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const btn = document.querySelector("#btn");
 
 function addAnswer(answerText, qIdx) {
-    let a = document.querySelector('.answerBox');
-    let answer = document.createElement('button');
-    answer.classList.add('answerList'); // answerList란 이름의 클래스값을 넣어줌 버튼에
-    a.appendChild(answer);
-    answer.innerHTML = answerText;
-    answer.addEventListener('click',() => {
-        let children = document.querySelectorAll('.answerList');
-        // 버튼 하나를 클릭하면 다른 버튼들은 무시가 되고 모든 버튼들이 사라지도록 만듦
+  let a = document.querySelector(".answerBox");
+  let answer = document.createElement("button");
+  answer.classList.add("answerList"); // answerList란 이름의 클래스값을 넣어줌 버튼에
+  answer.classList.add("my-3");
+  answer.classList.add("py-3");
+  answer.classList.add("mx-auto");
+  answer.classList.add("fadeIn");
+
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+  answer.addEventListener(
+    "click",
+    () => {
+      let children = document.querySelectorAll(".answerList");
+      // 버튼 하나를 클릭하면 다른 버튼들은 무시가 되고 모든 버튼들이 사라지도록 만듦
+      for (let i = 0; i < children.length; i++) {
+        children[i].disabled = true;
+        children[i].style.WebkitAnimation = "fadeOut 0.5s";
+        children[i].style.animation = "fadeOut 0.5s";
+      }
+      setTimeout(() => {
         for (let i = 0; i < children.length; i++) {
-            children[i].disabled = true;
-            children[i].style.display = 'none';
+          children[i].style.display = "none";
         }
         goNext(++qIdx);
-    })
+      }, 450);
+    },
+    false
+  );
 }
-function goNext(qIdx){
-    let q = document.querySelector('.qBox');
-    q.innerHTML = qnaList[qIdx].q;
-    for (let i in qnaList[qIdx].a) {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
-    }
+function goNext(qIdx) {
+  let q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
 }
 
 function begin() {
-    // main.style.WebkitAnimation = "fadeOut 1s";
-    main.style.animation = "fadeOut 1s";
+  main.style.WebkitAnimation = "fadeOut 1s";
+  main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
-        // qna.style.WebkitAnimation = "fadeIn 1s";
-        qna.style.animation = "fadeIn 1s";
-        setTimeout(() => {
-            main.style.display = "none";
-            qna.style.display = "block";
-        }, 450);
-        let qIdx = 0;
-        goNext(qIdx);
+      main.style.display = "none";
+      qna.style.display = "block";
     }, 450);
-    
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
 }
 
-
-
-btn.addEventListener('click', begin);
-
+btn.addEventListener("click", begin);
